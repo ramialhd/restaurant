5055f6b023dc84e86a6b49d18cb669803c0a5fd8
lolita86 <attayanagham@gmail.com>
2025-01-10 00:35:52 +0200

    working on food categorie

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('animateBtn').addEventListener('click', () => {
+    const img = document.querySelector('img');
+    img.style.transform = 'translateY(0)';
+    img.style.opacity = '1';
+  });
