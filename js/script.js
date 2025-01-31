ae5d4b8eea08afa2aefbd8adf121bf6f774657dc
lolita86 <attayanagham@gmail.com>
2025-02-01 00:23:00 +0200

    acess to an externale javascuprt file

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@ document.getElementById('animateBtn').addEventListener('click', () => {
     img.style.transform = 'translateY(0)';
     img.style.opacity = '1';
   });
+  const pastaImage = document.getElementById("pasta");
+
+    const hoverImageSrc = "images/laradrink2.jpeg";
+    const originalImageSrc = "images/pasta.jpg" ;
+
+    pastaImage.addEventListener("mouseover", () => {
+      pastaImage.style.opacity = 0; 
+      setTimeout(() => {
+        pastaImage.src = hoverImageSrc; 
+        pastaImage.style.opacity = 1;
+      }, 250);
+    });
+
+    pastaImage.addEventListener("mouseout", () => {
+      pastaImage.style.opacity = 0; 
+      setTimeout(() => {
+        pastaImage.src = originalImageSrc; 
+        pastaImage.style.opacity = 1; 
+      }, 250);
+    });
+    function myfunction() {
+      // Hide the form
+      document.getElementById("reservationForm").style.display = "none";
+      
+      // Show the thank-you message
+      document.getElementById("thankYouMessage").style.display = "block";
+  }
