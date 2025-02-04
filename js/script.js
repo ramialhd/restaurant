9f1b6ea0aa20cec06acc0b11ea0025ef2487a453
naghamattaya <attayanagham@gmail.com>
2025-02-04 18:20:04 +0200

    workng on reservation

--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
-document.getElementById('animateBtn').addEventListener('click', () => {
-    const img = document.querySelector('img');
-    img.style.transform = 'translateY(0)';
-    img.style.opacity = '1';
-  });
-  const pastaImage = document.getElementById("pasta");
 
-    const hoverImageSrc = "images/laradrink2.jpeg";
-    const originalImageSrc = "images/pasta.jpg" ;
-
-    pastaImage.addEventListener("mouseover", () => {
-      pastaImage.style.opacity = 0; 
-      setTimeout(() => {
-        pastaImage.src = hoverImageSrc; 
-        pastaImage.style.opacity = 1;
-      }, 250);
-    });
-
-    pastaImage.addEventListener("mouseout", () => {
-      pastaImage.style.opacity = 0; 
-      setTimeout(() => {
-        pastaImage.src = originalImageSrc; 
-        pastaImage.style.opacity = 1; 
-      }, 250);
-    });
-    function myfunction() {
-      // Hide the form
-      document.getElementById("reservationForm").style.display = "none";
-      
-      // Show the thank-you message
-      document.getElementById("thankYouMessage").style.display = "block";
-  }
+    
+      function myfunction() {
+        let firstName = document.forms["reservation"]["firstname"].value.trim();  // Corrected name
+        let lastName = document.forms["reservation"]["lastname"].value.trim();    // Corrected name
+        let guest = document.forms["reservation"]["number"].value;
+    
+        if (firstName === "" || lastName === "") {
+            alert("First name and last name cannot be empty");
+            return false;
+        }
+        if (guest > 30) {
+            alert("Number of guests must be at most 30");
+            return false;
+        }
+    
+        // Hide the form
+        document.getElementById("reservationForm").style.display = "none";
+    
+        // Show the thank-you message
+        document.getElementById("thankYouMessage").style.display = "block";
+        return true;
+    }
+    
